6f39481472bf7dcd6d59619975efdd1d936a332d
tri-setianto <tri.setianto@tokopedia.com>
2020-11-10 15:38:55 +0700

    add nsq

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	mnsq "github.com/sharring_session/nsq/nsq"
 
 	handlerhttp "github.com/sharring_session/nsq/http"
 )
 
 func main() {
 	fmt.Println("RUNNING")
+	mnsq.InitProducer()
 	handlerhttp.HandleRequests()
 }
